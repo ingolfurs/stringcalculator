a9f2d34c91aca29b6f8ba278f37be93d818c86ff
Ingólfur Sigfússon <ingolfur@stefna.is>
2014-10-17 23:28:09 +0000

    Test many new line seperators

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -27,11 +27,11 @@ public class CalculatorTest {
 	@Test
 	public void testMultipleNumbers(){
 		assertEquals(55, Calculator.add("1,2,3,4,5,6,7,8,9,10"));
-    	}
+	}
 
 	@Test
 	public void testNewLinesSeperators(){
-		assertEquals(6, Calculator.add("1\n2,3"));
-    	}
+		assertEquals(15, Calculator.add("1\n2,3\n4\n5"));
+	}
 
 }
